570450a62d37d1ac86f4fcb93c9141bb4611175d
Andrew Mitchell <andrew.mi@outlook.com.au>
2019-11-11 17:55:10 +0800

    Fixed Typo

--- PowerPointArtNet/PowerPointArtNet/Form1.Designer.cs
+++ PowerPointArtNet/PowerPointArtNet/Form1.Designer.cs
@@ -38,6 +38,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.StatusControl = new System.Windows.Forms.Label();
             this.DisconnectButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
@@ -45,9 +47,10 @@
             // IsEnabled
             // 
             this.IsEnabled.Enabled = false;
-            this.IsEnabled.Location = new System.Drawing.Point(33, 12);
+            this.IsEnabled.Location = new System.Drawing.Point(40, 14);
+            this.IsEnabled.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.IsEnabled.Name = "IsEnabled";
-            this.IsEnabled.Size = new System.Drawing.Size(190, 42);
+            this.IsEnabled.Size = new System.Drawing.Size(232, 50);
             this.IsEnabled.TabIndex = 0;
             this.IsEnabled.Text = "Connect";
             this.IsEnabled.UseVisualStyleBackColor = true;
@@ -55,20 +58,22 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(33, 101);
+            this.numericUpDown1.Location = new System.Drawing.Point(40, 121);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             256,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.Size = new System.Drawing.Size(147, 29);
             this.numericUpDown1.TabIndex = 1;
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(33, 172);
+            this.numericUpDown2.Location = new System.Drawing.Point(40, 206);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown2.Maximum = new decimal(new int[] {
             512,
             0,
@@ -80,7 +85,7 @@
             0,
             0});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2.Size = new System.Drawing.Size(147, 29);
             this.numericUpDown2.TabIndex = 2;
             this.numericUpDown2.Value = new decimal(new int[] {
             1,
@@ -92,18 +97,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 78);
+            this.label1.Location = new System.Drawing.Point(35, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.Size = new System.Drawing.Size(149, 25);
             this.label1.TabIndex = 3;
-            this.label1.Text = "ArcNet Universe";
+            this.label1.Text = "ArtNet Universe";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 149);
+            this.label2.Location = new System.Drawing.Point(35, 179);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.Size = new System.Drawing.Size(86, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Channel";
             // 
@@ -111,9 +118,10 @@
             // 
             this.NetworkControl.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.NetworkControl.FormattingEnabled = true;
-            this.NetworkControl.Location = new System.Drawing.Point(229, 99);
+            this.NetworkControl.Location = new System.Drawing.Point(280, 119);
+            this.NetworkControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.NetworkControl.Name = "NetworkControl";
-            this.NetworkControl.Size = new System.Drawing.Size(261, 28);
+            this.NetworkControl.Size = new System.Drawing.Size(318, 32);
             this.NetworkControl.TabIndex = 5;
             this.NetworkControl.DropDown += new System.EventHandler(this.NetworkControl_DropDown);
             this.NetworkControl.SelectionChangeCommitted += new System.EventHandler(this.NetworkControl_SelectionChangeCommitted);
@@ -121,18 +129,20 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(229, 78);
+            this.label3.Location = new System.Drawing.Point(280, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 20);
+            this.label3.Size = new System.Drawing.Size(157, 25);
             this.label3.TabIndex = 6;
             this.label3.Text = "Network Adapter";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(229, 149);
+            this.label4.Location = new System.Drawing.Point(280, 179);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(119, 20);
+            this.label4.Size = new System.Drawing.Size(147, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "Channel Status";
             // 
@@ -140,28 +150,50 @@
             // 
             this.StatusControl.AutoSize = true;
             this.StatusControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StatusControl.Location = new System.Drawing.Point(229, 177);
+            this.StatusControl.Location = new System.Drawing.Point(280, 212);
+            this.StatusControl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.StatusControl.Name = "StatusControl";
-            this.StatusControl.Size = new System.Drawing.Size(19, 20);
+            this.StatusControl.Size = new System.Drawing.Size(21, 24);
             this.StatusControl.TabIndex = 8;
             this.StatusControl.Text = "0";
             // 
             // DisconnectButton
             // 
             this.DisconnectButton.Enabled = false;
-            this.DisconnectButton.Location = new System.Drawing.Point(379, 21);
+            this.DisconnectButton.Location = new System.Drawing.Point(463, 25);
+            this.DisconnectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DisconnectButton.Name = "DisconnectButton";
-            this.DisconnectButton.Size = new System.Drawing.Size(133, 33);
+            this.DisconnectButton.Size = new System.Drawing.Size(163, 40);
             this.DisconnectButton.TabIndex = 9;
             this.DisconnectButton.Text = "Disconnect";
             this.DisconnectButton.UseVisualStyleBackColor = true;
             this.DisconnectButton.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(309, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Please See GitHub.com README";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(263, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "andrew-mi/PowerPointArtNet";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 224);
+            this.ClientSize = new System.Drawing.Size(658, 370);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.DisconnectButton);
             this.Controls.Add(this.StatusControl);
             this.Controls.Add(this.label4);
@@ -172,7 +204,8 @@
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.IsEnabled);
-            this.MinimumSize = new System.Drawing.Size(560, 280);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(679, 323);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "PowerPoint <--> ArtNet";
@@ -196,6 +229,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label StatusControl;
         private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
